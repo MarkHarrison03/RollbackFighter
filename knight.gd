8ff93008a82acd60d4c8f2aa3dec537558c821b2
Mark <21335265@studentmail.ul.ie>
2024-09-23 21:23:06 +0100

    new movement done

--- knight.gd
+++ knight.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @export var jump_force = -800
 @onready var knight : AnimationPlayer = $AnimationPlayer
 @onready var axis = Vector2.ZERO
-
+@onready var crouching = false
 func get_input_axis():
 	if is_on_floor():
 		axis.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
@@ -17,27 +17,43 @@ func _ready():
 	knight.active = true
 	
 func _physics_process(delta):
-
+	print(",")
 	axis = get_input_axis()
 	velocity.y += gravity * delta
 	horizontal_movement()
 	move_and_slide()
-	if is_on_floor() and axis.x == 0:
-		play_anim("Knight/Idle")
+
 	
 func horizontal_movement():
-	
+
 	var horizontal_input = Input.get_action_strength("right") - Input.get_action_strength("left")
-	if is_on_floor():
+	if is_on_floor() and !crouching:
 		velocity.x = horizontal_input * speed
 
-func _input(event):
-	if axis.y == 1:
+func _process(delta):
+	print("INPUT CALLED--------------------------")
+	print (axis.x)
+	print(axis.y)
+	print(is_on_floor_only())
+	if axis.x == 0 and axis.y == 0 and is_on_floor():
+		print("idle!")
+		play_anim("Knight/Idle")		
+	if crouching and is_on_floor():
 		play_anim("Crouch")
 	if axis.x == -1:
 		play_anim("Knight/BackWalk")
 	elif axis.x == 1:
 		play_anim("Knight/Walk")
+
+
+func _input(event):
+	if event.is_action_pressed("crouch") and is_on_floor():
+		print("CROUCHING")
+		axis.x = 0
+		velocity.x = 0
+		crouching = true
+	if event.is_action_released("crouch"):
+		crouching = false
 	if event.is_action_pressed("jump") and is_on_floor():
 		play_anim("NeutralJump")
 		if knight.current_animation != "NeutralJump":
